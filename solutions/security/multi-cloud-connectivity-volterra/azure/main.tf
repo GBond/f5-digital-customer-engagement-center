bd399522a4a3d6bd312c5b5c42ab54133f34e5cd
jeffgiroux <thejeffgiroux@gmail.com>
2021-04-13 16:57:24 -0700

    add for each loops

--- solutions/security/multi-cloud-connectivity-volterra/azure/main.tf
+++ solutions/security/multi-cloud-connectivity-volterra/azure/main.tf
@@ -2,326 +2,166 @@ provider "azurerm" {
   features {}
 }
 
-############################ Resource Groups ############################
+############################ Locals for Business Units ############################
 
-# bu11 Resource Group
-resource "azurerm_resource_group" "bu11" {
-  name     = format("%s-rgBu11-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location = var.azureLocation
-
-  tags = {
-    Name      = format("%s-rgBu11-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
+locals {
+  businessUnits = {
+    transitBu11 = {
+      name           = "transitBu11"
+      location       = var.azureLocation
+      addressSpace   = "100.64.48.0/20"
+      subnetPrefixes = ["100.64.48.0/24", "100.64.49.0/24", "100.64.50.0/24"]
+      subnetNames    = ["external", "internal", "workload"]
+    }
+    transitBu12 = {
+      name           = "transitBu12"
+      location       = var.azureLocation
+      addressSpace   = "100.64.64.0/20"
+      subnetPrefixes = ["100.64.64.0/24", "100.64.65.0/24", "100.64.66.0/24"]
+      subnetNames    = ["external", "internal", "workload"]
+    }
+    transitBu13 = {
+      name           = "transitBu13"
+      location       = var.azureLocation
+      addressSpace   = "100.64.80.0/20"
+      subnetPrefixes = ["100.64.80.0/24", "100.64.81.0/24", "100.64.82.0/24"]
+      subnetNames    = ["external", "internal", "workload"]
+    }
+    bu11 = {
+      name           = "bu11"
+      location       = var.azureLocation
+      addressSpace   = "10.1.0.0/16"
+      subnetPrefixes = ["10.1.10.0/24", "10.1.52.0/24"]
+      subnetNames    = ["external", "internal"]
+    }
+    bu12 = {
+      name           = "bu12"
+      location       = var.azureLocation
+      addressSpace   = "10.1.0.0/16"
+      subnetPrefixes = ["10.1.10.0/24", "10.1.52.0/24"]
+      subnetNames    = ["external", "internal"]
+    }
+    bu13 = {
+      name           = "bu13"
+      location       = var.azureLocation
+      addressSpace   = "10.1.0.0/16"
+      subnetPrefixes = ["10.1.10.0/24", "10.1.52.0/24"]
+      subnetNames    = ["external", "internal"]
+    }
   }
 }
-
-# bu12 Resource Group
-resource "azurerm_resource_group" "bu12" {
-  name     = format("%s-rgBu12-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location = var.azureLocation
-
-  tags = {
-    Name      = format("%s-rgBu12-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-}
-
-# bu13 Resource Group
-resource "azurerm_resource_group" "bu13" {
-  name     = format("%s-rgBu13-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location = var.azureLocation
-
-  tags = {
-    Name      = format("%s-rgBu13-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-}
-
-
-############################ VNets ############################
-
-module "vnetTransitBu11" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu11.name
-  vnet_name           = format("%s-vnetTransitBu11-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "100.64.48.0/20"
-  subnet_prefixes     = ["100.64.48.0/24", "100.64.49.0/24", "100.64.50.0/24"]
-  subnet_names        = ["external", "internal", "workload"]
-
-  tags = {
-    Name      = format("%s-vnetTransitBu11-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu11]
-}
-
-module "vnetTransitBu12" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu12.name
-  vnet_name           = format("%s-vnetTransitBu12-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "100.64.64.0/20"
-  subnet_prefixes     = ["100.64.64.0/24", "100.64.65.0/24", "100.64.66.0/24"]
-  subnet_names        = ["external", "internal", "workload"]
-
-  tags = {
-    Name      = format("%s-vnetTransitBu12-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu12]
-}
-
-module "vnetTransitBu13" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu13.name
-  vnet_name           = format("%s-vnetTransitBu13-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "100.64.80.0/20"
-  subnet_prefixes     = ["100.64.80.0/24", "100.64.81.0/24", "100.64.82.0/24"]
-  subnet_names        = ["external", "internal", "workload"]
-
-  tags = {
-    Name      = format("%s-vnetTransitBu13-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu13]
-}
-
-module "vnetBu11" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu11.name
-  vnet_name           = format("%s-vnetBu11-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "10.1.0.0/16"
-  subnet_prefixes     = ["10.1.10.0/24", "10.1.52.0/24"]
-  subnet_names        = ["external", "internal"]
-
-  tags = {
-    Name      = format("%s-vnetBu11-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu11]
-}
-
-module "vnetBu12" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu12.name
-  vnet_name           = format("%s-vnetBu12-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "10.1.0.0/16"
-  subnet_prefixes     = ["10.1.10.0/24", "10.1.52.0/24"]
-  subnet_names        = ["external", "internal"]
-
-  tags = {
-    Name      = format("%s-vnetBu12-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu12]
-}
-
-module "vnetBu13" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.bu13.name
-  vnet_name           = format("%s-vnetBu13-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  address_space       = "10.1.0.0/16"
-  subnet_prefixes     = ["10.1.10.0/24", "10.1.52.0/24"]
-  subnet_names        = ["external", "internal"]
-
-  tags = {
-    Name      = format("%s-vnetBu13-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  depends_on = [azurerm_resource_group.bu13]
-}
-
-
-############################ Route Tables ############################
-
-resource "azurerm_route_table" "transitBu11" {
-  name                          = format("%s-udrTransitBu11-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu11.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrTransitBu11-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
-resource "azurerm_route_table" "transitBu12" {
-  name                          = format("%s-udrTransitBu12-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu12.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrTransitBu12-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
-resource "azurerm_route_table" "transitBu13" {
-  name                          = format("%s-udrTransitBu13-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu13.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrTransitBu13-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
-resource "azurerm_route_table" "bu11" {
-  name                          = format("%s-udrBu11-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu11.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrBu11-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
-resource "azurerm_route_table" "bu12" {
-  name                          = format("%s-udrBu12-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu12.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrBu12-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
-resource "azurerm_route_table" "bu13" {
-  name                          = format("%s-udrBu13-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location                      = var.azureLocation
-  resource_group_name           = azurerm_resource_group.bu13.name
-  disable_bgp_route_propagation = false
-
-  route {
-    name                   = "volterra_gateway"
-    address_prefix         = "100.64.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.64.1.10"
-  }
-
-  tags = {
-    Name      = format("%s-udrBu13-%s", var.resourceOwner, random_id.buildSuffix.hex)
-    Terraform = "true"
-  }
-
-  #depends_on = [volterra_tf_params_action.applyBu11]
-}
-
 
 ############################ Locals for Compute ############################
 
 locals {
-
   jumphosts = {
-    bu11Jumphost = {
-      resourceGroup = azurerm_resource_group.bu11.name
-      subnet        = module.vnetBu11.vnet_subnets[0]
-    }
-    bu12Jumphost = {
-      resourceGroup = azurerm_resource_group.bu12.name
-      subnet        = module.vnetBu12.vnet_subnets[0]
-    }
-    bu13Jumphost = {
-      resourceGroup = azurerm_resource_group.bu13.name
-      subnet        = module.vnetBu13.vnet_subnets[0]
-    }
-    # transitBu11Jumphost = {
-    #   resourceGroup = azurerm_resource_group.bu11.name
-    #   subnet        = module.vnetBu11.vnet_subnets[0]
+    # transitBu11 = {
+    #   name   = "transitBu11"
+    #   subnet = module.network["transitBu11"].vnet_subnets[0]
     # }
-    # transitBu12Jumphost = {
-    #   resourceGroup = azurerm_resource_group.bu12.name
-    #   subnet        = module.vnetBu12.vnet_subnets[0]
+    # transitBu12 = {
+    #   name   = "transitBu12"
+    #   subnet = module.network["transitBu12"].vnet_subnets[0]
     # }
-    # transitBu13Jumphost = {
-    #   resourceGroup = azurerm_resource_group.bu13.name
-    #   subnet        = module.vnetBu13.vnet_subnets[0]
+    # transitBu13 = {
+    #   name   = "transitBu13"
+    #   subnet = module.network["transitBu13"].vnet_subnets[0]
     # }
+    bu11 = {
+      name   = "bu11"
+      subnet = module.network["bu11"].vnet_subnets[0]
+    }
+    bu12 = {
+      name   = "bu12"
+      subnet = module.network["bu12"].vnet_subnets[0]
+    }
+    bu13 = {
+      name   = "bu13"
+      subnet = module.network["bu13"].vnet_subnets[0]
+    }
   }
 
   webservers = {
-    bu11App1 = {
-      resourceGroup = azurerm_resource_group.bu11.name
-      subnet        = module.vnetBu11.vnet_subnets[1]
+    bu11 = {
+      name   = "bu11"
+      subnet = module.network["bu11"].vnet_subnets[1]
     }
-    bu12App1 = {
-      resourceGroup = azurerm_resource_group.bu12.name
-      subnet        = module.vnetBu12.vnet_subnets[1]
+    bu12 = {
+      name   = "bu12"
+      subnet = module.network["bu12"].vnet_subnets[1]
     }
-    bu13App1 = {
-      resourceGroup = azurerm_resource_group.bu13.name
-      subnet        = module.vnetBu13.vnet_subnets[1]
+    bu13 = {
+      name   = "bu13"
+      subnet = module.network["bu13"].vnet_subnets[1]
     }
   }
-
 }
 
+############################ Resource Groups ############################
+
+resource "azurerm_resource_group" "rg" {
+  for_each = local.businessUnits
+  name     = format("%s-rg-%s-%s", var.projectPrefix, each.value["name"], random_id.buildSuffix.hex)
+  location = each.value["location"]
+
+  tags = {
+    Name      = format("%s-rg-%s-%s", var.resourceOwner, each.value["name"], random_id.buildSuffix.hex)
+    Terraform = "true"
+  }
+}
+
+############################ VNets ############################
+
+module "network" {
+  for_each            = local.businessUnits
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.rg[each.value["name"]].name
+  vnet_name           = format("%s-vnet-%s-%s", var.projectPrefix, each.value["name"], random_id.buildSuffix.hex)
+  address_space       = each.value["addressSpace"]
+  subnet_prefixes     = each.value["subnetPrefixes"]
+  subnet_names        = each.value["subnetNames"]
+
+  tags = {
+    Name      = format("%s-vnet-%s-%s", var.resourceOwner, each.value["name"], random_id.buildSuffix.hex)
+    Terraform = "true"
+  }
+}
+
+############################ Route Tables ############################
+
+resource "azurerm_route_table" "rt" {
+  for_each                      = local.businessUnits
+  name                          = format("%s-rt-public-%s", var.projectPrefix, random_id.buildSuffix.hex)
+  location                      = azurerm_resource_group.rg[each.value["name"]].location
+  resource_group_name           = azurerm_resource_group.rg[each.value["name"]].name
+  disable_bgp_route_propagation = false
+
+  route {
+    name                   = "volterra_gateway"
+    address_prefix         = "100.64.0.0/16"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = "10.64.1.10"
+  }
+
+  tags = {
+    Name      = format("%s-rt-public-%s", var.resourceOwner, random_id.buildSuffix.hex)
+    Terraform = "true"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "rt" {
+  for_each       = local.businessUnits
+  subnet_id      = module.network[each.value["name"]].vnet_subnets[0]
+  route_table_id = azurerm_route_table.rt[each.value["name"]].id
+}
 
 ############################ Security Groups ############################
 
 resource "azurerm_network_security_group" "jumphost" {
   for_each            = local.jumphosts
-  name                = format("%s-nsgJumphost-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location            = var.azureLocation
-  resource_group_name = each.value["resourceGroup"]
+  name                = format("%s-nsg-jumphost-%s", var.projectPrefix, random_id.buildSuffix.hex)
+  location            = azurerm_resource_group.rg[each.value["name"]].location
+  resource_group_name = azurerm_resource_group.rg[each.value["name"]].name
 
   security_rule {
     name                       = "allow_SSH"
@@ -337,16 +177,16 @@ resource "azurerm_network_security_group" "jumphost" {
   }
 
   tags = {
-    Name      = format("%s-nsgJumphost-%s", var.resourceOwner, random_id.buildSuffix.hex)
+    Name      = format("%s-nsg-jumphost-%s", var.resourceOwner, random_id.buildSuffix.hex)
     Terraform = "true"
   }
 }
 
 resource "azurerm_network_security_group" "webserver" {
   for_each            = local.webservers
-  name                = format("%s-nsgWebservers-%s", var.projectPrefix, random_id.buildSuffix.hex)
-  location            = var.azureLocation
-  resource_group_name = each.value["resourceGroup"]
+  name                = format("%s-nsg-webservers-%s", var.projectPrefix, random_id.buildSuffix.hex)
+  location            = azurerm_resource_group.rg[each.value["name"]].location
+  resource_group_name = azurerm_resource_group.rg[each.value["name"]].name
 
   security_rule {
     name                       = "allow_SSH"
@@ -360,7 +200,6 @@ resource "azurerm_network_security_group" "webserver" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
   security_rule {
     name                       = "allow_HTTP"
     description                = "Allow HTTP access"
@@ -375,11 +214,10 @@ resource "azurerm_network_security_group" "webserver" {
   }
 
   tags = {
-    Name      = format("%s-nsgWebservers-%s", var.resourceOwner, random_id.buildSuffix.hex)
+    Name      = format("%s-nsg-webservers-%s", var.resourceOwner, random_id.buildSuffix.hex)
     Terraform = "true"
   }
 }
-
 
 ############################ Compute ############################
 
@@ -389,11 +227,11 @@ module "jumphost" {
   projectPrefix      = var.projectPrefix
   buildSuffix        = random_id.buildSuffix.hex
   resourceOwner      = var.resourceOwner
-  azureResourceGroup = each.value["resourceGroup"]
-  azureLocation      = var.azureLocation
+  azureResourceGroup = azurerm_resource_group.rg[each.value["name"]].name
+  azureLocation      = azurerm_resource_group.rg[each.value["name"]].location
   keyName            = var.keyName
   mgmtSubnet         = each.value["subnet"]
-  securityGroup      = azurerm_network_security_group.jumphost[each.key].id
+  securityGroup      = azurerm_network_security_group.jumphost[each.value["name"]].id
 }
 
 module "webserver" {
@@ -402,9 +240,9 @@ module "webserver" {
   projectPrefix      = var.projectPrefix
   buildSuffix        = random_id.buildSuffix.hex
   resourceOwner      = var.resourceOwner
-  azureResourceGroup = each.value["resourceGroup"]
-  azureLocation      = var.azureLocation
+  azureResourceGroup = azurerm_resource_group.rg[each.value["name"]].name
+  azureLocation      = azurerm_resource_group.rg[each.value["name"]].location
   keyName            = var.keyName
   subnet             = each.value["subnet"]
-  securityGroup      = azurerm_network_security_group.webserver[each.key].id
+  securityGroup      = azurerm_network_security_group.webserver[each.value["name"]].id
 }
--- solutions/security/multi-cloud-connectivity-volterra/azure/outputs.tf
+++ solutions/security/multi-cloud-connectivity-volterra/azure/outputs.tf
@@ -1,36 +1,36 @@
 output "bu11JumphostPublicIp" {
   description = "BU11 Jumphost Public IP"
-  value = module.jumphost[*].bu11Jumphost.publicIp
+  value       = module.jumphost[*].bu11.publicIp
 }
 output "bu12JumphostPublicIp" {
   description = "BU12 Jumphost Public IP"
-  value = module.jumphost[*].bu12Jumphost.publicIp
+  value       = module.jumphost[*].bu12.publicIp
 }
 output "bu13JumphostPublicIp" {
   description = "BU13 Jumphost Public IP"
-  value = module.jumphost[*].bu13Jumphost.publicIp
+  value       = module.jumphost[*].bu13.publicIp
 }
 output "bu11WebServerIP" {
   description = "BU11 Web Server Private IP"
-  value = module.webserver[*].bu11App1.privateIp
+  value       = module.webserver[*].bu11.privateIp
 }
 output "bu12WebServerIP" {
   description = "BU12 Web Server Private IP"
-  value = module.webserver[*].bu12App1.privateIp
+  value       = module.webserver[*].bu12.privateIp
 }
 output "bu13WebServerIP" {
   description = "BU13 Web Server Private IP"
-  value = module.webserver[*].bu13App1.privateIp
+  value       = module.webserver[*].bu13.privateIp
 }
 output "vnetIdBu11" {
   description = "BU11 VNet ID"
-  value       = module.vnetBu11.vnet_id
+  value       = module.network[*].bu11.vnet_id
 }
 output "vnetIdBu12" {
   description = "BU12 VNet ID"
-  value       = module.vnetBu12.vnet_id
+  value       = module.network[*].bu12.vnet_id
 }
 output "vnetIdBu13" {
   description = "BU13 VNet ID"
-  value       = module.vnetBu13.vnet_id
+  value       = module.network[*].bu13.vnet_id
 }
