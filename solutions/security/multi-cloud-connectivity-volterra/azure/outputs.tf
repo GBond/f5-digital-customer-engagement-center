c32022c90054b7c22bd07c6bd730baf5ea245823
jeffgiroux <thejeffgiroux@gmail.com>
2021-04-15 14:13:51 -0700

    fix outputs

--- solutions/security/multi-cloud-connectivity-volterra/azure/outputs.tf
+++ solutions/security/multi-cloud-connectivity-volterra/azure/outputs.tf
@@ -1,36 +1,48 @@
 output "bu11JumphostPublicIp" {
   description = "BU11 Jumphost Public IP"
-  value       = module.jumphost[*].bu11.publicIp
+  value       = module.jumphost["bu11"].publicIp
 }
 output "bu12JumphostPublicIp" {
   description = "BU12 Jumphost Public IP"
-  value       = module.jumphost[*].bu12.publicIp
+  value       = module.jumphost["bu12"].publicIp
 }
 output "bu13JumphostPublicIp" {
   description = "BU13 Jumphost Public IP"
-  value       = module.jumphost[*].bu13.publicIp
+  value       = module.jumphost["bu13"].publicIp
+}
+output "transitBu11JumphostPublicIp" {
+  description = "Transit BU11 Jumphost Public IP"
+  value       = module.jumphost["transitBu11"].publicIp
+}
+output "transitBu12JumphostPublicIp" {
+  description = "Transit BU12 Jumphost Public IP"
+  value       = module.jumphost["transitBu12"].publicIp
+}
+output "transitBu13JumphostPublicIp" {
+  description = "Transit BU13 Jumphost Public IP"
+  value       = module.jumphost["transitBu13"].publicIp
 }
 output "bu11WebServerIP" {
   description = "BU11 Web Server Private IP"
-  value       = module.webserver[*].bu11.privateIp
+  value       = module.webserver["bu11"].privateIp
 }
 output "bu12WebServerIP" {
   description = "BU12 Web Server Private IP"
-  value       = module.webserver[*].bu12.privateIp
+  value       = module.webserver["bu12"].privateIp
 }
 output "bu13WebServerIP" {
   description = "BU13 Web Server Private IP"
-  value       = module.webserver[*].bu13.privateIp
+  value       = module.webserver["bu13"].privateIp
 }
 output "vnetIdBu11" {
   description = "BU11 VNet ID"
-  value       = module.network[*].bu11.vnet_id
+  value       = module.network["bu11"].vnet_id
 }
 output "vnetIdBu12" {
   description = "BU12 VNet ID"
-  value       = module.network[*].bu12.vnet_id
+  value       = module.network["bu12"].vnet_id
 }
 output "vnetIdBu13" {
   description = "BU13 VNet ID"
-  value       = module.network[*].bu13.vnet_id
+  value       = module.network["bu13"].vnet_id
 }
