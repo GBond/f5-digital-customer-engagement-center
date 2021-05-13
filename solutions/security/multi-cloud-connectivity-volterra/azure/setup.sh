abb9daa152d30e0bc1a1f0692d8e259a3ceee6fe
jeffgiroux <thejeffgiroux@gmail.com>
2021-05-13 15:30:55 -0700

    setup and destroy script quick testing

--- solutions/security/multi-cloud-connectivity-volterra/azure/setup.sh
+++ solutions/security/multi-cloud-connectivity-volterra/azure/setup.sh
@@ -2,7 +2,7 @@
 terraform init
 terraform fmt
 terraform validate
-terraform plan
+#terraform plan
 # apply
-read -p "Press enter to continue"
+#read -p "Press enter to continue"
 terraform apply --auto-approve
